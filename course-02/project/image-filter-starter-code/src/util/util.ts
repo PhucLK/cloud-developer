68e0f3ce9b8f2ed7c6fe50bc5eb959a68073d647
phuclk <phuclkcarryonto04@gmail.com>
2022-11-01 06:36:11 +0700

    update package.json

--- course-02/project/image-filter-starter-code/src/util/util.ts
+++ course-02/project/image-filter-starter-code/src/util/util.ts
@@ -1,5 +1,5 @@
-import fs from "fs";
-import Jimp from "jimp";
+import fs from 'fs'
+import Jimp from 'jimp'
 
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
@@ -8,23 +8,23 @@ import Jimp from "jimp";
 //    inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
-export async function filterImageFromURL(inputURL: string): Promise<string> {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
-        .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
-          resolve(__dirname + outpath);
-        });
-    } catch (error) {
-      reject(error);
-    }
-  });
+export async function filterImageFromURL(inputURL: string | any): Promise<string> {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const photo = await Jimp.read(inputURL);
+            const outpath =
+                "\\tmp\\filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+            await photo
+                .resize(256, 256) // resize
+                .quality(60) // set JPEG quality
+                .greyscale() // set greyscale
+                .write(__dirname + outpath, (img) => {
+                    resolve(__dirname + outpath);
+                });
+        } catch (error) {
+            reject(error);
+        }
+    });
 }
 
 // deleteLocalFiles
@@ -33,7 +33,19 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files: Array<string>) {
-  for (let file of files) {
-    fs.unlinkSync(file);
-  }
+    for (let file of files) {
+        fs.unlinkSync(file);
+    }
+}
+
+
+export function isValidImageUrl(url: string | any): boolean {
+    const validUrl = require('valid-url');
+    try {
+        return validUrl.isUri(url)
+    } catch (error) {
+        console.log('error here /....');
+        return false
+    }
+
 }
